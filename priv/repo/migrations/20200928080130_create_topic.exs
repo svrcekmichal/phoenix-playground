f3611f3699da184a177754ff8d709966181df237
Michal Svrcek <michal@aurity.co>
2020-09-28 10:04:01 +0200

    test ecto migration with gigalixir

---
+++ priv/repo/migrations/20200928080130_create_topic.exs
@@ -0,0 +1,11 @@
+defmodule Playground.Repo.Migrations.CreateTopic do
+  use Ecto.Migration
+
+  def change do
+    create table(:topics) do
+      add :title, :string
+      add :description, :string
+      timestamps()
+    end
+  end
+end
