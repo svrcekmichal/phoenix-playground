791f1d090f6ea957e865f7e43a796ff968ac2bf2
Michal Svrcek <michal@aurity.co>
2020-09-28 20:16:22 +0200

    fix formating

--- lib/playground_web/controllers/auth_controller.ex
+++ lib/playground_web/controllers/auth_controller.ex
@@ -9,7 +9,7 @@ defmodule PlaygroundWeb.AuthController do
     |> handle_sign_in(conn)
   end
 
-  defp make_changeset(%{ assigns: %{ ueberauth_auth: auth } }, %{ "provider" => provider }) do
+  defp make_changeset(%{assigns: %{ueberauth_auth: auth}}, %{"provider" => provider}) do
     User.changeset(%User{}, %{
       name: auth.info.name,
       username: auth.info.nickname,
@@ -21,19 +21,22 @@ defmodule PlaygroundWeb.AuthController do
   end
 
   defp upsert_user(changeset) do
-    Playground.Repo.insert(changeset, on_conflict: { :replace, [:name, :token, :avatar] }, conflict_target: :username)
+    Playground.Repo.insert(changeset,
+      on_conflict: {:replace, [:name, :token, :avatar]},
+      conflict_target: :username
+    )
   end
 
-  defp handle_sign_in({ :ok, user }, conn) do
+  defp handle_sign_in({:ok, user}, conn) do
     conn
-      |> put_flash(:info, "You've been logged in")
-      |> put_session(:user_id, user.id)
-      |> redirect(to: Routes.topic_path(conn, :index))
+    |> put_flash(:info, "You've been logged in")
+    |> put_session(:user_id, user.id)
+    |> redirect(to: Routes.topic_path(conn, :index))
   end
 
   defp handle_sign_in({:err, _}, conn) do
     conn
-      |> put_flash(:error, "Error signing in")
-      |> redirect(to: Routes.topic_path(conn, :index))
+    |> put_flash(:error, "Error signing in")
+    |> redirect(to: Routes.topic_path(conn, :index))
   end
 end
