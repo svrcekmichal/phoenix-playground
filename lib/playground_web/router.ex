2fb78d9a15be52ed99552c3dc95c0ca3727af002
Michal Svrcek <michal@aurity.co>
2020-09-28 21:15:09 +0200

    set user plug and login/logout button

--- lib/playground_web/router.ex
+++ lib/playground_web/router.ex
@@ -7,6 +7,7 @@ defmodule PlaygroundWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Playground.Plugs.SetUser
   end
 
   pipeline :api do
