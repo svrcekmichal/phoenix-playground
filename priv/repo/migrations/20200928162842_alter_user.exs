da9d43eb007eb028d2e63e818b712fbbe20e60f6
Michal Svrcek <michal@aurity.co>
2020-09-28 19:58:58 +0200

    fix missing migration

--- priv/repo/migrations/20200928162842_alter_user.exs
+++ priv/repo/migrations/20200928162842_alter_user.exs
@@ -2,9 +2,12 @@ defmodule Playground.Repo.Migrations.AlterUser do
   use Ecto.Migration
 
   def change do
-    alter table(:users) do
+    create table(:users) do
+      add :name, :string
       add :provider, :string
       add :token, :string
+      add :email, :string
+      timestamps()
     end
   end
 end
